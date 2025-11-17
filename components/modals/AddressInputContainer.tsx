e5ac07d5df73d2e790e7a1913cdd8af690e8de31
aidanj2m <aidanj2m@gmail.com>
2025-11-17 15:27:49 -0500

    build fix

--- components/modals/AddressInputContainer.tsx
+++ components/modals/AddressInputContainer.tsx
@@ -111,6 +111,8 @@ export default function AddressInputContainer({
               city: '',
               state: '',
               zip_code: '',
+              latitude: undefined,
+              longitude: undefined,
             });
           });
 
